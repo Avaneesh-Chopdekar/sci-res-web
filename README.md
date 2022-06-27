4d0e44940bcf6703064cf68274c3cead61237bc1
Avaneesh Chopdekar <avaneeshchopdekar42@gmail.com>
2022-06-27 20:53:46 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+
+# Science Resources
+
+I made this website so I can have all of these books on the go.
+
+
+![Logo](https://scienceresourcesjee.tk/sci_books_logo.png)
+
+
+## Link to page
+
+[Visit Site](https://scienceresourcesjee.web.app/)
---
+++ vite.config.js
@@ -0,0 +1,23 @@
+const { resolve } = require("path");
+
+module.exports = {
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        boards: resolve(__dirname, "boards/index.html"),
+        eleventh: resolve(__dirname, "boards/11th/index.html"),
+        twelveth: resolve(__dirname, "boards/12th/index.html"),
+        info: resolve(__dirname, "info/index.html"),
+        pyqs: resolve(__dirname, "pyqs/index.html"),
+        phypyqs: resolve(__dirname, "pyqs/phy/index.html"),
+        mathspyqs: resolve(__dirname, "pyqs/maths/index.html"),
+        chempyqs: resolve(__dirname, "pyqs/chem/index.html"),
+        jee: resolve(__dirname, "jee/index.html"),
+        phyjee: resolve(__dirname, "jee/phy/index.html"),
+        mathsjee: resolve(__dirname, "jee/maths/index.html"),
+        chemjee: resolve(__dirname, "jee/chem/index.html"),
+      },
+    },
+  },
+};
